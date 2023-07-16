42c215b1bd9187734d5cf021177f49e405eb6c50
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-16 10:29:13 -0700

    edited index and vite config

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+// vite.config.ts
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: 'https://ramitbratabiswas.github.io/three-js-sphere/'
+})
